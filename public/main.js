44b78fb6e86b3242f6fa6f6be0ad6e965c17bf55
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-04-01 00:48:14 +0200

    updated main to test canvas

--- public/main.js
+++ public/main.js
@@ -1,3 +1,15 @@
-"use strict";
-var i = 0;
-i++;
+import { resize_canvas } from "./canvas.js";
+import { populateBoard } from "./board.js";
+let cnv;
+let gridSize = 9;
+let board = [[]];
+window.onload = function () {
+    board = populateBoard(gridSize);
+    console.log("works");
+    cnv = document.getElementById("myCanvas");
+    //cnv.addEventListener("mouseup", (event) => {gameLoop(event, [window.innerWidth, window.innerHeight], cnv);});
+    resize_canvas([window.innerWidth, window.innerHeight], cnv, gridSize, board);
+};
+window.onresize = function () {
+    resize_canvas([window.innerWidth, window.innerHeight], cnv, gridSize, board);
+};
