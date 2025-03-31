4713b695c751a340de626eb0169e12cff5c4d20c
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-04-01 01:21:53 +0200

    added cell type

--- public/board.js
+++ public/board.js
@@ -3,7 +3,8 @@ export function populateBoard(gridSize) {
     for (let i = 0; i < gridSize; ++i) {
         board[i] = [];
         for (let j = 0; j < gridSize; ++j) {
-            board[i][j] = Math.floor(Math.random() * gridSize) + 1;
+            board[i][j] = { num: 0, possibilities: [0], selected: false };
+            board[i][j].num = Math.floor(Math.random() * gridSize) + 1;
         }
     }
     return board;
--- src/board.ts
+++ src/board.ts
@@ -1,9 +1,16 @@
+export type cell = {
+    num:number,
+    possibilities:number[],
+    selected:boolean
+};
+
 export function populateBoard(gridSize:number) {
-    let board:number[][] = [];
+    let board:cell[][] = [];
     for(let i = 0; i < gridSize; ++i) {
         board[i] = [];
         for(let j = 0; j < gridSize; ++j) {
-            board[i][j] = Math.floor(Math.random() * gridSize) + 1;
+            board[i][j] = {num:0, possibilities:[0], selected:false};
+            board[i][j].num = Math.floor(Math.random() * gridSize) + 1;
         }
     }
     return board;
