6bf9eb1312b8050f57e511f01b3816a6157570a2
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-04-01 02:17:45 +0200

    changed default state of cell possibilities

--- src/board.ts
+++ src/board.ts
@@ -9,7 +9,7 @@ export function populateBoard(gridSize:number) {
     for(let i = 0; i < gridSize; ++i) {
         board[i] = [];
         for(let j = 0; j < gridSize; ++j) {
-            board[i][j] = {num:0, possibilities:[0], selected:false};
+            board[i][j] = {num:0, possibilities:[-1], selected:false};
             board[i][j].num = Math.floor(Math.random() * gridSize) + 1;
         }
     }
