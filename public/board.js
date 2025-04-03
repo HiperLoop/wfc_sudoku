e70c47571dce80867847991160d8a8bb51e217ec
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-04-03 22:14:04 +0200

    added possible states to generated cells

--- public/board.js
+++ public/board.js
@@ -3,8 +3,9 @@ export function populateBoard(gridSize) {
     for (let i = 0; i < gridSize; ++i) {
         board[i] = [];
         for (let j = 0; j < gridSize; ++j) {
-            board[i][j] = { num: 0, possibilities: [-1], selected: false };
-            board[i][j].num = Math.floor(Math.random() * gridSize) + 1;
+            const temp = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            board[i][j] = { num: 0, possibilities: temp, selected: false };
+            board[i][j].num = Math.floor(Math.random() * 2) >= 1 ? Math.floor(Math.random() * gridSize) + 1 : 0;
         }
     }
     return board;
--- src/board.ts
+++ src/board.ts
@@ -1,6 +1,6 @@
 export type cell = {
     num:number,
-    possibilities:number[],
+    possibilities:Set<number>,
     selected:boolean
 };
 
@@ -9,8 +9,9 @@ export function populateBoard(gridSize:number) {
     for(let i = 0; i < gridSize; ++i) {
         board[i] = [];
         for(let j = 0; j < gridSize; ++j) {
-            board[i][j] = {num:0, possibilities:[-1], selected:false};
-            board[i][j].num = Math.floor(Math.random() * gridSize) + 1;
+            const temp:Set<number> = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            board[i][j] = {num:0, possibilities:temp, selected:false};
+            board[i][j].num = Math.floor(Math.random() * 2) >= 1 ?  Math.floor(Math.random() * gridSize) + 1 : 0;
         }
     }
     return board;
