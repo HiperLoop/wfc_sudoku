929b2e13fba7c87d778cfc7499e3f5320f771081
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-04-03 22:14:49 +0200

    added click functionality to show degrees of freedom rather than values of cells

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
-import { resize_canvas } from "./canvas.js";
+import { resize_canvas, drawBoard } from "./canvas.js";
 import { populateBoard } from "./board.js";
+import { solve } from "./solver.js";
 let cnv;
 let gridSize = 9;
 let board = [[]];
@@ -7,7 +8,7 @@ window.onload = function () {
     board = populateBoard(gridSize);
     //console.log("works");
     cnv = document.getElementById("myCanvas");
-    //cnv.addEventListener("mouseup", (event) => {gameLoop(event, [window.innerWidth, window.innerHeight], cnv);});
+    cnv.addEventListener("mouseup", (event) => { solve(board, gridSize); drawBoard(board, gridSize, cnv, [window.innerWidth, window.innerHeight], true); });
     resize_canvas([window.innerWidth, window.innerHeight], cnv, gridSize, board);
 };
 window.onresize = function () {
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { resize_canvas } from "./canvas.js";
+import { resize_canvas, drawBoard } from "./canvas.js";
 import { populateBoard , cell } from "./board.js";
+import { solve } from "./solver.js";
 
 let cnv:HTMLCanvasElement;
 let gridSize = 9;
@@ -9,7 +10,7 @@ window.onload = function() {
     board = populateBoard(gridSize);
     //console.log("works");
     cnv = <HTMLCanvasElement> document.getElementById("myCanvas");
-    //cnv.addEventListener("mouseup", (event) => {gameLoop(event, [window.innerWidth, window.innerHeight], cnv);});
+    cnv.addEventListener("mouseup", (event) => {solve(board, gridSize); drawBoard(board, gridSize, cnv, [window.innerWidth, window.innerHeight], true)});
     resize_canvas([window.innerWidth, window.innerHeight], cnv, gridSize, board);
 }
 
