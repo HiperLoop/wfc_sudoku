f012a34873fe78ec66a57ac7fbe585e847be3853
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-04-04 17:05:05 +0200

    added a medium difficulty test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { resize_canvas, drawBoard } from "./canvas.js";
 import { populateBoard , cell } from "./board.js";
 import { generateDegreesOfFreedom, solve } from "./solver.js";
-import { test_one , cellBoardFromValues , easy } from "./tests.js";
+import { test_one , cellBoardFromValues , easy , medium } from "./tests.js";
 
 let cnv:HTMLCanvasElement;
 let gridSize = 9;
@@ -9,8 +9,9 @@ let board:cell[][] = [[]];
 
 window.onload = function() {
     //board = populateBoard(gridSize);
-    board = test_one;
+    //board = test_one;
     //board = cellBoardFromValues(easy);
+    board = cellBoardFromValues(medium);
     //console.log("works");
     cnv = <HTMLCanvasElement> document.getElementById("myCanvas");
     cnv.addEventListener("mouseup", (event) => {solve(board, gridSize, [window.innerWidth, window.innerHeight], cnv); drawBoard(board, gridSize, cnv, [window.innerWidth, window.innerHeight], true)});
