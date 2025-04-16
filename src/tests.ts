15ef3b3e66dce4c6fb892af66b241960d1762365
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2025-04-16 18:03:29 +0200

    added another test to tests

--- src/tests.ts
+++ src/tests.ts
@@ -11,6 +11,20 @@ import { cell } from "./board.js"
 [{num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:8, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:5, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:1, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}],
 [{num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:9, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:4, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}, {num:0, possibilities:new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]), selected:false}]];
  */
+
+export const test_one:number[][] = [
+    [8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 6, 0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 9, 0, 2, 0, 0],
+
+    [0, 5, 0, 0, 0, 7, 0, 0, 0],
+    [0, 0, 0, 0, 4, 5, 7, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 3, 0],
+
+    [0, 0, 1, 0, 0, 0, 0, 6, 8],
+    [0, 0, 8, 5, 0, 0, 0, 1, 0],
+    [0, 9, 0, 0, 0, 0, 4, 0, 0]];
+
 export const easy:number[][] = [
     [0, 8, 0, 0, 0, 0, 2, 1, 3],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -32,6 +46,59 @@ export const medium:number[][] = [
     [0, 5, 0, 0, 6, 0, 0, 0, 0],
     [0, 0, 1, 0, 0, 2, 0, 9, 0],
     [8, 0, 0, 0, 0, 0, 0, 7, 1]];
+
+export const sofia:number[][][] = [
+    [[0, 0, 0, 4, 9, 0, 8, 0, 0],
+    [0, 7, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 9, 8, 7, 2, 0, 0, 3],
+    [4, 0, 2, 3, 6, 0, 1, 8, 9],
+    [0, 0, 8, 9, 4, 5, 2, 3, 7],
+    [9, 3, 0, 2, 1, 8, 6, 4, 5],
+    [7, 4, 0, 1, 0, 9, 5, 6, 8],
+    [0, 9, 1, 6, 0, 3, 7, 2, 0],
+    [0, 8, 6, 7, 5, 4, 3, 0, 0]],
+
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]],
+
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]],
+
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+];
+
+export const shion:number[][] = [
+    [7, 0, 0, 2, 6, 0, 0, 5, 0],
+    [0, 0, 5, 0, 0, 3, 0, 0, 7],
+    [0, 9, 0, 0, 8, 7, 0, 0, 0],
+    [1, 0, 0, 0, 4, 0, 7, 9, 0],
+    [2, 0, 7, 1, 0, 9, 3, 0, 6],
+    [0, 4, 9, 0, 2, 0, 0, 0, 8],
+    [0, 0, 0, 6, 3, 0, 0, 7, 0],
+    [9, 0, 0, 8, 0, 0, 4, 0, 0],
+    [0, 7, 0, 0, 1, 4, 0, 0, 3]];
 
 export function cellBoardFromValues(values:number[][]) {
     let board:cell[][] = [];
